89fd65ff00d1db31f934e6592df7844f0ba12bc9
JV-Martins <jvkm2001@gmail.com>
2021-03-15 22:16:30 -0300

    Progreção do App

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { AccountComponent } from './views/account/account.component';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent
+  },
+  {
+    path:"account",
+    component: AccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,37 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null as any
+  }
+
+  constructor(private productService: ProductService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  createProduct(): void{
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Product saved!')
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel(){
+    this.router.navigate(['/products']);
+    this.productService.showMessage('Operation canceled!');
+  }
+
+}
