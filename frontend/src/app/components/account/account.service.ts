010721c942d0fcaed11eb81a45d964029545266b
JV-Martins <jvkm2001@gmail.com>
2021-03-22 15:59:15 -0300

    Final da aplicação

--- frontend/src/app/components/account/account.service.ts
+++ frontend/src/app/components/account/account.service.ts
@@ -14,12 +14,12 @@ export class AccountService {
   constructor(private snackBar: MatSnackBar,
               private http: HttpClient) { }
 
-  showMessage(msg: string){
+  showMessage(msg: string, isError: boolean = false){
     this.snackBar.open(msg,'X',{
       duration:2000,
       horizontalPosition:"right",
       verticalPosition: "top",
-      panelClass: "msg-success"
+      panelClass: isError ? ['msg-error']: ['msg-success']
     })
   }
 
